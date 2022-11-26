8f50e603d49f275701effbe6d7dc4efe86bb99a7
Lucas Dantas da Cruz <ldantascruz@gmail.com>
2022-11-25 21:39:24 -0300

    update files - remove unnecessary files

---
+++ lib/common/constants/app_colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+
+  static const Color greenlightOne = Color(0xFF63B5AF);
+  static const Color greenlightTwo = Color(0xFF438883);
+  static const Color white = Color(0xFFFFFFFF);
+
+  static const List<Color> greenGradient = [
+    Color(0xFF63B5AF),
+    Color(0xFF438883)
+  ];
+}
---
+++ lib/features/splash/spash_page.dart
@@ -0,0 +1,29 @@
+import 'package:financy_app/common/constants/app_colors.dart';
+import 'package:financy_app/common/constants/app_text_styles.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: AppColors.greenGradient,
+          ),
+        ),
+        child: Text(
+          'financy',
+          style: AppTextStyles.bigtext.copyWith(
+            color: AppColors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
